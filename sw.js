5b0f6c297da47d0b7ff3981aeeeb26e04cc84c42
Maor <maor561@gmail.com>
2026-07-18 14:41:50 +0300

    bump: cache version to v5.2

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * the platform's servers); text-to-speech and tap-to-check work fully offline.
  */
 
-const CACHE = 'smart-checklist-v5.1';
+const CACHE = 'smart-checklist-v5.2';
 
 const SHELL = [
   './',
